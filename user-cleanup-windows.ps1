cc136ce7f6ff079650914f1569ef4b8f2ca41a68
Jeremiah(KD7DMP) <121139459+KD7DMP@users.noreply.github.com>
2023-10-27 11:06:58 -0700

    Update user-cleanup-windows.ps1
    I've made some tweeks to deal with accounts that don't have a IconCache.db file.
    I think these are very old accounts, or are remote access acounts.  I'll try teams as our org loads that by default on every profile.

--- user-cleanup-windows.ps1
+++ user-cleanup-windows.ps1
@@ -4,7 +4,7 @@
 Set-ExecutionPolicy Bypass
 #first we grab a list of all the users on the system
 $userlist = Get-CimInstance -ClassName win32_userprofile |Select-Object localpath, lastusetime, Loaded
-
+$old = $false
 #then we parse out the user names and check to see how old they are
 #due to a bug in Windows 10 post update 1709 when system updates are done it updates ALL NTUSER.DAT files. So you can't user that as a measuer of when they last logged in.
 #so I use the IconCache.db file. Thanks to the folks on this thread for finding this. https://techcommunity.microsoft.com/t5/windows-deployment/issue-with-date-modified-for-ntuser-dat/m-p/102438
@@ -13,15 +13,21 @@ foreach ($user in $userlist) {
     if ($user.localpath.StartsWith("C:\Users\")) {
         $userArray = $user.localpath.Split("\")
         $userName = $userArray[2]
-        $Dat = Get-Item "C:\Users\$userName\AppData\Local\IconCache.db" -Force
-        $DatTime = $Dat.LastWriteTime
-        $age = (Get-Date).adddays(-375)
-        if ($DatTime -lt $age){
-            $old = "Old"
+        $fileExists = Test-Path "C:\Users\$userName\AppData\Local\IconCache.db"
+        if ($fileExists){
+            $Dat = Get-Item "C:\Users\$userName\AppData\Local\IconCache.db" -Force
+        }else{
+            $Dat = Get-Item "C:\Users\$userName\AppData\Local\Microsoft\Teams\" -Force
         }
-        Write-Host $old
+            $DatTime = $Dat.LastWriteTime
+            $age = (Get-Date).adddays(-375)
+            if ($DatTime -lt $age){
+                $old = $true
+                Write-Host $old
+            }
+        
         #Now we remove this user from the system We make sure to ask the user before deleting a user just to be sure.
-        if ($user.loaded -eq $false -and $old -eq "Old"){
+        if ($user.loaded -eq $false -and $old){
                 $answer = Read-Host $userName, $DatTime, "Do you want to Delete this user? yes/no"
                 
                 if ($answer -eq 'yes'){
@@ -31,8 +37,10 @@ foreach ($user in $userlist) {
                     Write-Host $userName, $DatTime, "No Action, User Skiped"
                 }           
         }
-        Elseif($user.loaded -and $old -eq "Old"){
+        Elseif($user.loaded -and $old){
             Write-Host $userName, $DatTime, "This is old, just still loaded. Leave it alone"
+        }Elseif($userName -eq "pcmaint"){
+            Write-Host $userName, $DatTime,"We Will leave pcmaint be"
         }
         else{
         Write-Host $userName, $DatTime, $old
