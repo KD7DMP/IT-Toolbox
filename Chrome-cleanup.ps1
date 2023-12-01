df8222b4ae8835c13dee1195838118147479617e
Jeremiah(KD7DMP) <121139459+KD7DMP@users.noreply.github.com>
2023-12-01 08:34:04 -0800

    Some updates to the comments
    updates to the comments so I know what the hell I was doing.

--- Chrome-cleanup.ps1
+++ Chrome-cleanup.ps1
@@ -1,5 +1,5 @@
 #This scrip will look at the regesty and pull all of the current user profiles, Then it will look for a Chrome install in the user's appdata folder. If it's older than 90 days
-#the chrome application directory will be deleted.
+#the chrome will be updated using the google update tool 
 
 Set-ExecutionPolicy Bypass
 #first we grab a list of all the users on the system
@@ -13,8 +13,10 @@ foreach ($user in $userlist) {
     if ($user.localpath.StartsWith("C:\Users\")) {
         $userArray = $user.localpath.Split("\")
         $userName = $userArray[2]
+        #Check to see if the local install of chrome is present
         $fileExists = Test-Path "C:\Users\$userName\AppData\Local\Google\Chrome\Application\chrome.exe"
         if ($fileExists){
+            #if it is figure out how old it is.
             $Dat = Get-Item "C:\Users\$userName\AppData\Local\Google\Chrome\Application\chrome.exe" -Force 
             $DatTime = $Dat.LastWriteTime
             $age = (Get-Date).adddays(-60)
@@ -24,12 +26,15 @@ foreach ($user in $userlist) {
                 Write-Host $old
             }
             if ($old) {
+                #If it's old, run the local google update to schedule an update. This should happen in the background and the user will be prompted to relunch when it's finished. 
             Invoke-Command -Scriptblock {"C:\Users\$userName\AppData\Local\Google\Update\GoogleUpdate.exe /ua /installsource scheduler"}
         }else {
+            #if it's not old then say as much.
             Write-Host $userName" Chrome is up to date."
         }
         $old = $false
         }else{
+            #if we can't find the local version then say it's on the globa install.
             Write-Output $userName"Is using the global chrome, no action taken."
         }
         Write-Host $old
